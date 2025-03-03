9d1343b9473d262ceecf792b4f799f904b4f3338
hackathon2024 <mfloresherbas00@gmail.com>
2025-03-03 08:14:07 -0400

    fix core

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { ConfigService } from "@nestjs/config"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const origin =  configService.get<String>("CORS_ORIGIN")?.split(",")
-  console.log(origin)
-  // app.enableCors({
-  //   origin: origin // Permitir solo solicitudes de este origen
-  // });
+  const origin = configService.get<string>("CORS_ORIGIN")?.split(",").map(origin => origin.trim()); // Elimina espacios extra
+
+  console.log("CORS Origins:", origin); // Para depuración
+
+  app.enableCors({
+    origin: origin, // Usamos el array limpio
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  });
 
   const port = 3001; // Define el puerto
   await app.listen(port); 
