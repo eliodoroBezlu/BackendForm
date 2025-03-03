a74d9f10da4c1e5f8da19bb65126ed5e1cd4fcd4
hackathon2024 <mfloresherbas00@gmail.com>
2025-03-02 22:41:57 -0400

    delete cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { ConfigService } from "@nestjs/config"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.enableCors({
-    origin: configService.get<String>("CORS_ORIGIN")?.split(","), // Permitir solo solicitudes de este origen
-  });
+  const origin =  configService.get<String>("CORS_ORIGIN")?.split(",")
+  console.log(origin)
+  // app.enableCors({
+  //   origin: origin // Permitir solo solicitudes de este origen
+  // });
 
   const port = 3001; // Define el puerto
   await app.listen(port); 
